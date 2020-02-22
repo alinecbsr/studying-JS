e0a65e861c332ee0d9b7c3c23d5f374f9623230b
Carvalho, Aline De <aline.de.carvalho@accenture.com>
2020-02-22 15:31:23 -0300

    Refatorado '00-tiposDeDados' para melhor visualização em console

--- 00-tiposDeDados.js
+++ 00-tiposDeDados.js
@@ -1,41 +1,43 @@
-/* 
+console.log(`
 ## Tipos de Dados
 
 
-### Primitivos */
+### Primitivos
 
-// - boolean
+- boolean:  \n`);
 
 console.log(typeof true);
 console.log(typeof false);
 
-// - number
+console.log(`\n - number: \n`);
 
 console.log(typeof 29);
 console.log(typeof 1.5);
 console.log(typeof 0xffff);
 
-// - string
+console.log(`\n - string \n`);
 
 console.log(typeof "aspas simples");
 console.log(typeof "aspas duplas");
 console.log(typeof `backticks`);
 
-// - null (tem valor atribuído, mas esse valor é vazio, é nada)
+console.log(
+  `\n - null (tem valor atribuído, mas esse valor é vazio, é nada) \n`
+);
 
 console.log(typeof null);
 
-// - undefined  (não há nenhuma atribuição de valor)
+console.log(`\n - undefined  (não há nenhuma atribuição de valor) \n`);
 
 console.log(typeof undefined);
 
-// - symbol
+console.log(`\n - symbol  \n`);
 
 console.log(typeof Symbol("symbol"));
 
-// ### Outros
-
-// - object
+console.log(`\n ### Outros
+ 
+- object  \n`);
 
 console.log(
   typeof {
