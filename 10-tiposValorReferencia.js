e130db65b26f2f073926180f88f7187d1a2f365b
Carvalho, Aline De <aline.de.carvalho@accenture.com>
2020-02-24 14:57:23 -0300

    Tipos: Valor / Referencia

---
+++ 10-tiposValorReferencia.js
@@ -0,0 +1,7 @@
+console.log(`
+
+- Tipo de valor: quando você passa uma cópia do valor de certa variável para a função. 
+O valor dentro da variável original que foi enviada não é alterado mesmo após a execução da função.
+
+- Tipo de referência: quando você passa a referência do objeto para uma função. 
+As alterações feitas ao objeto nessa função também ocorrem com o objeto fora dela. `);
