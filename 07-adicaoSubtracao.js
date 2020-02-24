e1d7966d53631af64ba7b5a11068bb28173b28c8
Carvalho, Aline De <aline.de.carvalho@accenture.com>
2020-02-24 09:49:36 -0300

    Adição e Subtração

---
+++ 07-adicaoSubtracao.js
@@ -0,0 +1,26 @@
+console.log(`
+## Adição e subtração
+
+`);
+
+console.log("simples\n");
+console.log(1 + 1);
+console.log(1 + 0.1);
+console.log(1 - 1);
+console.log(1 - 0.1);
+
+console.log("\n\ncom variável\n");
+let a = 0;
+console.log(a);
+a += 5;
+// a = a + 5;
+console.log(a);
+a++;
+console.log(a);
+
+let b = 10;
+console.log(b);
+b -= 5;
+console.log(b);
+b--;
+console.log(b);
